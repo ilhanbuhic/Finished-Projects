0efe40c47a1a9f357f8ad4ecb14ec969b0a685cc
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-06-03 23:47:34 +0200

    Pig gme

--- Codding Challange - Pig game/index.js
+++ Codding Challange - Pig game/index.js
@@ -42,3 +42,35 @@ function init() {
   }
   init()
   
+  function switchPlayer() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+  }
+  
+  // Rolling dice functionality
+  btnRoll.addEventListener('click', function () {
+    if (playing) {
+      // 1. Generating a random dice roll
+      const dice = Math.trunc(Math.random() * 6) + 1
+  
+      // 2. Display dice
+      diceEl.classList.remove('hidden')
+      diceEl.src = `./images/dice-${dice}.png`
+  
+      // 3.Check for rolled 1: if true, switch to next player
+      if (dice !== 1) {
+        // Add dice to current score
+        currentScore += dice
+        document.getElementById(`current--${activePlayer}`).textContent =
+          currentScore
+      } else {
+        // Switch to next player
+        switchPlayer()
+      }
+    }
+  })
+  
+
