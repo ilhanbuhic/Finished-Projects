0693713e6f545e4f48890a2868eeed275956fd7d
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-06-08 21:06:53 +0200

    Adding features

--- Tic Tac Toe/index.js
+++ Tic Tac Toe/index.js
@@ -16,20 +16,47 @@ const winningSequence = [
 
 let currentPlayer = 'X'
 let gameEnded = false // Track if the game has ended
+let modal = document.querySelector('.modal')
+let overlay = document.querySelector('.overlay')
+let textNodeWin = document.createTextNode(`Player ${currentPlayer} wins!`)
+let modalP = document.querySelector('.modalP')
+let restartBtn = document.querySelector('.restartBtn')
+let player1Name = document.querySelector('.player1--name')
+let player2Name = document.querySelector('.player2--name')
+let player1Score = document.querySelector('.player1--score')
+let player2Score = document.querySelector('.player2--score')
+let scores = [0, 0]
+
+restartBtn.addEventListener('click', function () {
+  resetGame()
+  overlay.classList.add('hidden')
+  modal.classList.add('hidden')
+})
+
+// displayModal()
+// const displayModal = function () {
+//   modalP.textContent = `Player ${currentPlayer} wins!`
+//   overlay.classList.remove('hidden')
+//   modal.classList.remove('hidden')
+// }
 
 const switchPlayer = function () {
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
 }
 
+const gridMain = document.querySelector('.grid')
 const grid = document.querySelectorAll('.grid > div')
 
-grid.forEach(function (gridEl) {
+grid.forEach(function (gridEl, index) {
   gridEl.addEventListener('click', function () {
     if (!gridEl.textContent && !gameEnded) {
       gridEl.textContent = currentPlayer
       if (checkWinner(currentPlayer)) {
-        alert(`Player ${currentPlayer} wins`)
+        gridEl.textContent === 'X'
+        // alert(`Player ${currentPlayer} wins`)
         gameEnded = true
+        // displayModal()
+        resetGame()
       } else if (checkDraw()) {
         alert(`It's a draw`)
         gameEnded = true
@@ -48,6 +75,15 @@ const checkWinner = function (player) {
       grid[b].textContent === player &&
       grid[c].textContent === player
     ) {
+      if (player === 'X') {
+        scores[0]++
+        player1Score.textContent = `Score: ${scores[0]}`
+        console.log(scores)
+      } else {
+        scores[1]++
+        player2Score.textContent = `Score: ${scores[1]}`
+        console.log(scores)
+      }
       return true
     }
   }
