5fa0130a6ecb100d12cdb53f1f969b7cdbfe2f3b
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-01-09 18:18:28 +0100

    add classes and textContent to DOM

--- Searchbar - Find User/app.js
+++ Searchbar - Find User/app.js
@@ -1,21 +1,29 @@
+const body = document.body
+const usersDiv = document.createElement('div')
+body.appendChild(usersDiv)
+
+const ulTag = document.createElement('ul')
+ulTag.className = 'users'
+usersDiv.appendChild(ulTag)
+
 const users = {
-    user1: {firstName: 'Ilhan'},
-    user2: {firstName: 'Carl'},
-    user3: {firstName: 'Lupe'},
-    user4: {firstName: 'Katherine'},
-    user5: {firstName: 'Kelly'},
-    user6: {firstName: 'Kurt'},
-    user7: {firstName: 'Fred'},
-    user8: {firstName: 'Chris'},
-    user9: {firstName: 'Per'},
+  //   user1: 'a',
+  user1: { firstName: 'Ilhan' },
+  user2: { firstName: 'Carl' },
+  user3: { firstName: 'Lupe' },
+  user4: { firstName: 'Katherine' },
+  user5: { firstName: 'Kelly' },
+  user6: { firstName: 'Kurt' },
+  user7: { firstName: 'Fred' },
+  user8: { firstName: 'Chris' },
+  user9: { firstName: 'Per' },
 }
 
-for (let [key, value] of users) {
-    console.log(key);
+for (let [key, user] of Object.entries(users)) {
+  const liTag = document.createElement('li')
+  ulTag.appendChild(liTag)
+  liTag.textContent = user.firstName
+  for (let [key, value] of Object.entries(user)) {
+    liTag.className = key
+  }
 }
-
-document.getElementById('search-input').addEventListener('keyup', function(event) {
-    let seachQuery = event.target.value.toLowerCase()
-    let allNameDOMCollection = document.getElementsByClassName('firstName')
-})
-``
