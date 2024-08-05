bd915c1640b9a31d33239b85b3cc710ac4dc345c
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-08-05 20:31:25 +0200

    add leaflet map, add css, add logic for map & marker

--- trackmyruns/script.js
+++ trackmyruns/script.js
@@ -14,3 +14,41 @@ const months = [
   'November',
   'December',
 ]
+
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords
+
+      const coords = [latitude, longitude]
+
+      var map = L.map('map').setView(coords, 14)
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map)
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup()
+      })
+    },
+    function () {
+      alert('Could not get location')
+    }
+  )
